949e3050f26700d99fd26b1498484e1263af1790
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-08-05 15:44:26 +0600

    something

--- class_calculatior.py
+++ class_calculatior.py
@@ -1,11 +1,13 @@
 class Calculator:
+
     def __init__(self,x,y):
         self.x=x
         self.y=y
+
     def plus(self):
         xx = x+y
         print(xx)
 
 
 obje = Calculator(10,20)
-obje.plus
+obje.plus(5,4)
