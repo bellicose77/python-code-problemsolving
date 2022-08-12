73b3e59e3410f33712a31c0a93ab233ee59be4a7
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-08-12 13:45:07 +0600

    linked list

--- Linked_list.py
+++ Linked_list.py
@@ -2,6 +2,9 @@ class Node:
     def __init__(self,data,next=None):
         self.data=data
         self.next=next
+class LinkedList:
+    def __init__(self):
+        self.head=None
 first = Node(3)
 print(first.data)
 print(first.next)
