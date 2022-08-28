2c8036cebc99f9b4ca3964c547ea6bce495cdc2b
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-08-28 11:27:30 +0600

    linkedlist

--- 1721A_Image.py
+++ 1721A_Image.py
@@ -1,9 +1,21 @@
 t=int(input())
 def image(x,y):
-    print(x)
-    print(y)
+    #print("xxsx")
+    z=x+y
+    re = ''.join(sorted(z))
+    #print("re",re)
+    l=0
+    s=1
+    while l!=3:
+        if(re[l]!=re[l+1]):
+            #print("koto bar l",l)
+            s+=1
+            #print("ss",s)
+        l+=1
+    return s
 while t!=0:
     x=input()
     y=input()
-    image(x,y)
+    result = image(x,y)
+    print(result-1)
     t-=1
