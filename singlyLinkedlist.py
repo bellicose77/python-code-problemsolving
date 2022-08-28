7f088259138a5878b023283f9c34cc8052ec37ae
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-08-28 14:44:24 +0600

    triangle

--- singlyLinkedlist.py
+++ singlyLinkedlist.py
@@ -1,11 +1,11 @@
 #node = int(input("Enter number of node: "))
 class Node:
-    def __init__(self,data,next=None):
+    def __init__(self,data):
         self.data=data
-        self.next=next
+        self.next=None
 
 class LinkedList:
-    def __init__(self,next=None):
+    def __init__(self):
         self.head=next
     def printLinked(self):
         if self.head is None:
@@ -14,10 +14,16 @@ class LinkedList:
             n=self.head
             while n is not None:
                 print(n.data)
- 
                 n=n.next
-ll1 = LinkedList(3)
+    def add_begin(self,data):
+        new_node = Node(data)
+        new_node.next=self.head
+        self.head=new_node
+ll1 = LinkedList()
+ll1.add_begin(10)
+ll1.add_begin(20)
 ll1.printLinked()
+
 # while node!=0:
 #     value = input()
 #     headnode = Node(value)
