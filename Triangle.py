419aa7c6b87891de7468e0dfeed5e4d7ac2d9aef
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-08-28 14:53:24 +0600

    triangle done

--- Triangle.py
+++ Triangle.py
@@ -3,4 +3,10 @@ class Triangle:
     self.left=a
     self.right=b
     self.height=c
+
+   def premiter (self):
+    result = self.left+self.right+self.height
+    return result
 triangleOne = Triangle(5,6,7)
+x=triangleOne.premiter()
+print(x)
