30099756b1409e350aae1810f1a39d3a84075f23
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-08-29 16:26:07 +0600

    data add begin done

--- final_likedlist.py
+++ final_likedlist.py
@@ -8,9 +8,16 @@ class LinkedList:
     def print_list(self):
         if self.head==None:
             print("Emptly linked list")
+        n=self.head
+        while n is not None:
+            print(n.data)
+            n=n.next
     def add_begin(self,data):
         new_node=Node(data)
         new_node.next=self.head
         self.head=new_node
 ll1=LinkedList()
+ll1.add_begin(5)
 ll1.add_begin(10)
+ll1.add_begin(15)
+ll1.print_list()
