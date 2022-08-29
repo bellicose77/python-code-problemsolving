e04f1c1db8b60d1c72fd4b3c9aa25552863de907
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-08-29 16:05:29 +0600

    final

--- singlyLinkedlist.py
+++ singlyLinkedlist.py
@@ -6,7 +6,7 @@ class Node:
 
 class LinkedList:
     def __init__(self):
-        self.head=next
+        self.head=None
     def printLinked(self):
         if self.head is None:
             print("Empty linked list")
@@ -14,14 +14,30 @@ class LinkedList:
             n=self.head
             while n is not None:
                 print(n.data)
+                print(n.next)
                 n=n.next
     def add_begin(self,data):
         new_node = Node(data)
         new_node.next=self.head
         self.head=new_node
+    def add_end(self,data):
+        new_node = Node(data)
+        if self.head==None:
+            self.head=new_node
+        else:
+            while self.head.next==None:
+                print("loop e dukhce",self.head.data)
+                self.head.next=new_node
+                self.head=new_node
+                #self.head=new_node
+
+
 ll1 = LinkedList()
-ll1.add_begin(10)
-ll1.add_begin(20)
+#ll1.add_begin(10)
+ll1.add_end(10)
+ll1.add_end(20)
+ll1.add_end(30)
+ll1.add_end(40)
 ll1.printLinked()
 
 # while node!=0:
