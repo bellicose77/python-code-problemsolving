3a698b024ccf060787d5f17978309bf65f3ac600
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-08-29 16:17:46 +0600

    linked listA

--- final_likedlist.py
+++ final_likedlist.py
@@ -3,4 +3,11 @@ class Node:
         self.data=data
         self.next=None
 class LinkedList:
-    pass
+    def __init__(self):
+        self.head=None
+    def add_begin(self,data):
+        new_node=Node(data)
+        new_node.next=self.head
+        self.head=new_node
+ll1=LinkedList()
+ll1.add_begin(10)
