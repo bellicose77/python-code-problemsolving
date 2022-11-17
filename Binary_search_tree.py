953a5482c4607471f738fc484fdcca16c56b6dd7
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-11-17 20:50:46 +0600

    all function

--- Binary_search_tree.py
+++ Binary_search_tree.py
@@ -3,3 +3,10 @@ class TreeNode:
         self.data=data
         self.left=None
         self.right=None
+class Tree:
+    def __init__(self):
+        pass
+    def insert_node(self):
+        pass
+    def print_tree(self):
+        pass
