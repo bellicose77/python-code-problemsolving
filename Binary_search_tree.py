f6602d9fa6f48732cdadd3221fc8c3c29a8b1d7b
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-11-19 23:48:45 +0600

    inseration function done

--- Binary_search_tree.py
+++ Binary_search_tree.py
@@ -12,13 +12,25 @@ class Tree:
             self.root = TreeNode(data)
             return 
         current_node=self.root
+        #print(current_node.data)
         previous_node = None
+        #print(previous_node)
+        while current_node:
+            previous_node=current_node
+            if data < current_node.data:
+                current_node=current_node.left
+            else :
+                current_node=current_node.right
+        if data <previous_node.data:
+            previous_node.left=TreeNode(data)
+        else:
+            previous_node.right = TreeNode(data)
+        
 
     def print_tree(self):
         pass
     def build_tree(self):
         for x in [7, 6, 8, 5, 9, 2, 12, 3]:
             self.insert_node(x)
-n1 = Tree
-n1.build_tree
-print(n1.data)
+n1 = Tree()
+n1.build_tree()
