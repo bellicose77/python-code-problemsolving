bad1858be8928235c149a7c6ddf64b447657f85f
bellicose77 <dulalcse1877@gmail.com>
2023-04-07 09:51:22 +0600

    insertation method

--- BST_Loop.py
+++ BST_Loop.py
@@ -7,6 +7,15 @@ class BST:
     def __init__(self):
         self.root=None
     def insert_node(self,data):
+        if self.root is None:
+            self.root = TreeNode(data)
+            return
+        temp=self.root
+        while True:
+            
+    def build_bst(self):
+        for x in [7,6,8,3,5]:
+            self.insert_node(x)
 n1=TreeNode()
 n1.BST()
 
