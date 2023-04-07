d91fc975e1ef2e624724f13fadce634c5f2722c2
bellicose77 <dulalcse1877@gmail.com>
2023-04-07 09:44:14 +0600

    insertation method

--- BST_Loop.py
+++ BST_Loop.py
@@ -3,3 +3,8 @@ class TreeNode:
         self.data=data
         self.left=None
         self.right=None
+class BST:
+    def __init__(self):
+        self.root=None
+    def insert_node(self,data):
+        
